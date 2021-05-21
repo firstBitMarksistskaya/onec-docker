e430162a38f304f793b779a6e8d7e6f082a1a191
d.abramov <dmitry.abramov@bellerage.ru>
2021-05-21 23:03:40 +0300

    Адаптация образа сервера под все версии платформы

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [ "$1" = "ragent" ]; then
+if [ "$1" = "ragent" -a "$onec_8318plus_version" = true ]; then
+  exec gosu usr1cv8 /opt/1cv8/x86_64/$onec_ver/ragent
+elif [ "$1" = "ragent" ]; then
   exec gosu usr1cv8 /opt/1C/v8.3/x86_64/ragent
 fi
 
